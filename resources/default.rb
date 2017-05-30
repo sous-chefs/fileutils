ba77316f1de12da60389b95717ed97283c031681
markgibbons <mark.gibbons@nordstrom.com>
2017-05-29 23:13:59 -0700

    UIS-3138: Style fixes

--- resources/default.rb
+++ resources/default.rb
@@ -39,11 +39,11 @@ action :create do
   changed = update_files(new_resource.path, new_resource.pattern, new_resource.recursive, new_resource.follow_symlink,
                          new_resource.directory_mode, new_resource.file_mode, new_resource.group, new_resource.owner,
                          new_resource.only_files, new_resource.only_directories, Chef::Config[:why_run])
-  converge_by("Update file #{new_resource.name}") { } if changed
+  converge_by("Update file #{new_resource.name}") {} if changed
 end
 
 action :delete do
   changed = delete_files(new_resource.path, new_resource.pattern, new_resource.follow_symlink,
                          new_resource.only_files, new_resource.force, Chef::Config[:why_run])
-  converge_by("Delete files #{new_resource.name}") { } if changed
+  converge_by("Delete files #{new_resource.name}") {} if changed
 end
